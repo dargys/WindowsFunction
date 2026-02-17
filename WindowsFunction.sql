48590412e0887f94aa0809250e06548b23579c2d
Dargys <diperez0494@gmail.com>
2026-02-17 08:39:18 +0100

    FirstExcersice

---
+++ WindowsFunction.sql
@@ -0,0 +1,9 @@
+/*1. Ta fram anställda och visa FullName, land och hur många som bor i respektive land. 
+Detta ska ske utan Group By. Vad blir skillnaden jft med att ha en Group By med i frågan?*/
+SELECT 
+	CONCAT(FirstName,' ',LastName),
+	Country,
+	COUNT(EmployeeID)OVER(PARTITION BY Country)AntalPerLand
+FROM Employees
+
+-- Jag skulle inte kunna visa detaljerad information med GROUP BY. 
